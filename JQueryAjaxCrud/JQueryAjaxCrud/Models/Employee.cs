adc06b084436afbdb22c4aa0a56841fc829b1e6a
Sunil <sunil_karmakar@ymail.com>
2021-09-19 12:09:35 +0600

    Working with DB
    1. Create Employee Domain class.
    2. Create EmployeeDbContext class.
    3. Update Startup class.
    4. Run Migration.

---
+++ JQueryAjaxCrud/JQueryAjaxCrud/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JQueryAjaxCrud.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please provied Name.")]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set;  }
+
+        [Required(ErrorMessage = "Please provied valid email address.")]
+        [StringLength(50, MinimumLength = 11)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please provied Department Name.")]
+        [Display(Name = "Department Name")]
+        [StringLength(20, MinimumLength = 2)]
+        public string DepartmentName { get; set;  }
+
+        [Required(ErrorMessage = "Please provied valid Account Number.")]
+        [Display(Name = "Account Number")]
+        [StringLength(20, MinimumLength = 8)]
+        [DataType(DataType.PhoneNumber)]
+        public string AccountNumber { get; set; }
+    }
+}
--- JQueryAjaxCrud/JQueryAjaxCrud/Startup.cs
+++ JQueryAjaxCrud/JQueryAjaxCrud/Startup.cs
@@ -1,5 +1,7 @@
+using JQueryAjaxCrud.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,11 @@ namespace JQueryAjaxCrud
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<EmployeeDbContext>(optionsAction =>
+            {
+                optionsAction.UseSqlServer(Configuration.GetConnectionString("DevConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
