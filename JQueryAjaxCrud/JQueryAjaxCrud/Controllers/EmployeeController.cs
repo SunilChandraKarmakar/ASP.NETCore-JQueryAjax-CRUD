b159722be999256c868a1fc8b796950eaf16f8f9
Sunil <sunil_karmakar@ymail.com>
2021-09-19 15:03:24 +0600

    Working with Controller and View
    1. Create Employee Controller.
    2. Create Index and Upsert function and view.
    3. Update Startup class.
    4. Update _layout file.

---
+++ JQueryAjaxCrud/JQueryAjaxCrud/Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using JQueryAjaxCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace JQueryAjaxCrud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDbContext _db; 
+
+        public EmployeeController(EmployeeDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Employees.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Upsert(int id = 0)
+        {
+            if (id == 0)
+                return View();
+            else
+            {
+                Employee existEmployee = await _db.Employees.FindAsync(id);
+
+                if (existEmployee == null)
+                    return NotFound();
+
+                return View(existEmployee);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Employees.Add(employee);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index)); 
+            }
+
+            return  View(employee);
+        }
+    }
+}
--- JQueryAjaxCrud/JQueryAjaxCrud/Startup.cs
+++ JQueryAjaxCrud/JQueryAjaxCrud/Startup.cs
@@ -53,7 +53,7 @@ namespace JQueryAjaxCrud
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Employee}/{action=Index}/{id?}");
             });
         }
     }
